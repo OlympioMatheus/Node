4daeadf74d03fb66f63e8222df08ea5c598cde60
Olympio Carvalho <ocarvalho@APLICACAO1.BR>
2023-12-20 16:08:48 -0300

    Projeto Node

--- 2_FUNDAMENTOS/9_lendo_input/index.js
+++ 2_FUNDAMENTOS/9_lendo_input/index.js
@@ -4,10 +4,9 @@ const readline =  require("readline").createInterface({
 })
 
 readline.question("Qual a sua linguagem preferida? ", (language) => {
-    if(language === 'Python'){
-    console.log("Isso nem é linguagem")
-    }
-    else {
+    if(language === "python"){
+    console.log('Isso nem é linguagem')
+    } else {
         console.log(`A minha linguagem preferida é: ${language} `)
     } 
     readline.close()  
